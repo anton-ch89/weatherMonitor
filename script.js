c66075d976f1258431322bc8393403400b8e52b6
Anton Chudinov <anton.ch89@gmail.com>
2021-09-13 13:07:37 +0500

    Fix

--- script.js
+++ script.js
@@ -15,21 +15,21 @@ window.addEventListener('load', () => {
         const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
             minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
             seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
-        document.querySelector('.new-date').textContent = hours  + ':' + minutes + ':' + seconds;
-        if( hours >= 0  && hours <= 4){
+        document.querySelector('.new-date').textContent = hours + ':' + minutes + ':' + seconds;
+        if (hours >= 0 && hours <= 4) {
             body.classList.add('night');
-        }else if(hours >= 5  && hours <= 9){
+        } else if (hours >= 5 && hours <= 9) {
             body.classList.add('sunset');
-        }else if(hours >= 10  && hours <= 17){
+        } else if (hours >= 10 && hours <= 17) {
             body.classList.add('day');
-        }else if(hours >= 18  && hours <= 23){
+        } else if (hours >= 18 && hours <= 23) {
             body.classList.add('sunset');
         }
     };
     setInterval(clock, 1000);
     clock();
 
-    
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -39,29 +39,29 @@ window.addEventListener('load', () => {
             const api = `http://api.weatherapi.com/v1/current.json?key=b16d149a0f7540f989b190424210909&q=${latet},${longit}&aqi=no`;
 
             fetch(api)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            const {current, location} = data;
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { current, location } = data;
 
-            timezone.textContent = location.tz_id;
-            temperatureValue.textContent = current.temp_c;
-            description.textContent = current.condition.text.toUpperCase();
-            console.log(current.condition.icon);
-            locationClass.insertAdjacentHTML(`beforeend`, `<img src='${current.condition.icon}'/>`);
-            valueSection.addEventListener('click', () => {
-                if(degree.textContent === '°C') {
-                    degree.textContent = '°F';
-                    temperatureValue.textContent = current.temp_f;
-                } else {
-                    degree.textContent = '°C';
+                    timezone.textContent = location.tz_id;
                     temperatureValue.textContent = current.temp_c;
-                }
-            });
-        });
+                    description.textContent = current.condition.text.toUpperCase();
+                    console.log(current.condition.icon);
+                    locationClass.insertAdjacentHTML(`beforeend`, `<img src='${current.condition.icon}'/>`);
+                    valueSection.addEventListener('click', () => {
+                        if (degree.textContent === '°C') {
+                            degree.textContent = '°F';
+                            temperatureValue.textContent = current.temp_f;
+                        } else {
+                            degree.textContent = '°C';
+                            temperatureValue.textContent = current.temp_c;
+                        }
+                    });
+                });
         });
     }
-    
+
 });
